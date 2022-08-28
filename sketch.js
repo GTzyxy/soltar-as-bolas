aa94b100a1c3cbf3b8b828d4e782df3b2e223f29
Bia <82560900+BiatrizR@users.noreply.github.com>
2022-08-27 21:10:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,10 @@ const Body = Matter.Body;
 
 var rectangle1, rectangle2, rectangle3;
 
-var angle = 60
+//criar variaveis de angulos diferentes, com diferentes valores, ou com os mesmo valores de angulo
+var angle = 60;
+var angle1 = 60;
+var angle2 = 60;
 function preload()
 {
 	
@@ -19,9 +22,10 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	//Crie os Corpos aqui.
-	var rectangle_option = { restitution:0.4, friction:0.02 }
-    rectangle1 = Bodies.rectangle(250, 200, 150, 20, rectangle_option);
+	//faltou definir o corpo como estatico
+	var rectangle_option = { isStatic: true,
+				restitution:0.4, friction:0.02 }
+        rectangle1 = Bodies.rectangle(250, 200, 150, 20, rectangle_option);
 	rectangle2 = Bodies.rectangle(250, 200, 150, 20, rectangle_option);
 	rectangle3 = Bodies.rectangle(250, 200, 150, 20, rectangle_option);
 
@@ -40,14 +44,23 @@ function draw() {
   rectMode(CENTER);
   background("lightgreen");
  
-  Matter.Body.rotate(rectangle1,angle);
+  
   push();
   translate(rectangle1.position.x, rectangle1.position.y);
   rotate(angle);
+  rect(0,0, 150, 20);
   pop();
-  
   angle +=0.2;
-  rect(rectangle1.position.x, rectangle1.position.y, 150, 20);
+	
+//com angulos diferentes é possível criar outras configurações de reatngulos entre as funções push() e pop()
+  push();
+  translate(rectangle2.position.x, rectangle2.position.y);
+  rotate(angle2);
+  rect(0,0, 150, 20);
+  pop();
+  angle +=0.5;
+	
+ 
   rect(rectangle2.position.x, rectangle2.position.y, 200, 20);
   rect(rectangle3.position.x, rectangle3.position.y, 250, 20);
 
